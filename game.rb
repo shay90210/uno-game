397b010d957ebbc5ca7734d8b802ff4fd0d20d1d
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-22 14:08:49 +0000

    added capitalize method to gets chomp and created new statement

--- game.rb
+++ game.rb
@@ -5,7 +5,8 @@ class Game
     def begin_game
         puts "Welcome to Uno!"
         puts "What is your name?"
-        player_name = gets.chomp
+        player_name = gets.chomp.capitalize
         puts "\n"
+        puts "Welcome #{self.name}!"
     end
 end
