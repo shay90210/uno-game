3a38519d9d5eebdbb88651da9bc2e80918565ad4
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-02 04:39:34 +0000

    removed comment from shuffle_deck - not needed anymore

--- card_deck.rb
+++ card_deck.rb
@@ -8,7 +8,6 @@ class Card_Deck
     def shuffle_deck
        # once the cards are shuffled - see if there is a way to stack cards or confirm that the deck is ready
        @cards.shuffle
-       #print cards
        print @cards.length
     end
 
